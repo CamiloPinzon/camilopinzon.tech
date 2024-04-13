f9a8066a9dccc1d72665d09ed672653043fb56ca
Camilo Pinzon <pinzonac@gmail.com>
2024-04-13 18:01:44 -0500

    divisor component

---
+++ src/components/divisor/divisor.component.tsx
@@ -0,0 +1,5 @@
+import "./divisor.styles.scss";
+
+const Divisor = () => <div className="divisor"></div>;
+
+export default Divisor;
--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,11 +1,13 @@
 import Greeting from "../../components/greeting/Greeting.component";
 import Introduction from "../../components/introduction/introduction.component";
+import Divisor from "../../components/divisor/divisor.component";
 
 const AboutPage = () => {
 	return (
 		<main className="about-page">
 			<Greeting />
 			<Introduction />
+			<Divisor />
 		</main>
 	);
 };
