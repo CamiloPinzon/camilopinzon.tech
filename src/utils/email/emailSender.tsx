0add2953e968da19e3d8812168639a612075d86f
Camilo Pinzon <pinzonac@gmail.com>
2024-04-24 18:09:41 -0500

    env vars

--- src/utils/email/emailSender.tsx
+++ src/utils/email/emailSender.tsx
@@ -8,15 +8,21 @@ const EmailSender = async (emailData: IContactForm) => {
 		email: emailData.email,
 		message: emailData.message,
 	};
-	emailjs.init("EarehDA7tjBLCIlpG");
-	emailjs.send("service_zctf1go", "template_ytxrmit", templateParams).then(
-		(response) => {
-			console.log("SUCCESS!", response.status, response.text);
-		},
-		(error) => {
-			console.log("FAILED...", error);
-		}
-	);
+	emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+	emailjs
+		.send(
+			import.meta.env.VITE_EMAILJS_SERVICE_ID,
+			import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+			templateParams
+		)
+		.then(
+			(response) => {
+				console.log("SUCCESS!", response.status, response.text);
+			},
+			(error) => {
+				console.log("FAILED...", error);
+			}
+		);
 };
 
 export default EmailSender;
