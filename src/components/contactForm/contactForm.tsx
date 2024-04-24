b6044c221232ac57b0fb91a23f2f39edcfb951f2
Camilo Pinzon <pinzonac@gmail.com>
2024-04-24 15:59:52 -0500

    Add emailJS

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -3,7 +3,7 @@ import { ReCAPTCHA } from "react-google-recaptcha";
 
 import InputComponent from "../inputComponent/inputComponent";
 import MainButton from "../mainButton/mainButton";
-//import EmailSender from "../../utils/email/emailSender";
+import EmailSender from "../../utils/email/emailSender";
 
 import { IContactForm } from "../../utils/interfaces";
 
@@ -35,7 +35,7 @@ const ContactForm = () => {
 			console.error("reCaptcha validation failed!");
 			return;
 		} else {
-			console.log('Send email');
+			EmailSender(contactForm);
 			
 		}
 	};
--- src/utils/email/emailSender.tsx
+++ src/utils/email/emailSender.tsx
@@ -1,10 +1,27 @@
+import emailjs from "@emailjs/browser";
+
 import { IContactForm } from "../interfaces.tsx";
-//import ContactEmailTemplate from "./contactEmailTemplate/contactEmailTemplate.tsx";
 
 const EmailSender = async (emailData: IContactForm) => {
-	const { name, email, message } = emailData;
-	console.log( name, email, message);
-	
+	const templateParams = {
+		name: emailData.name,
+		email: emailData.email,
+		message: emailData.message,
+	};
+	emailjs
+		.send(
+			process.env.EMAILJS_SERVICE_ID!,
+			process.env.EMAILJS_TEMPLATE_ID!,
+			templateParams
+		)
+		.then(
+			(response) => {
+				console.log("SUCCESS!", response.status, response.text);
+			},
+			(error) => {
+				console.log("FAILED...", error);
+			}
+		);
 };
 
 export default EmailSender;
