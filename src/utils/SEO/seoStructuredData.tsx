9cfa314d58e86868e492b6ff029d7be4d0e6ca99
Camilo Pinzon <pinzonac@gmail.com>
2024-04-24 22:39:51 -0500

    Add facebook meta tags

--- src/utils/SEO/seoStructuredData.tsx
+++ src/utils/SEO/seoStructuredData.tsx
@@ -30,14 +30,29 @@ export const aboutStructuredData = () => (
 		<meta
 			name="keywords"
 			content="web developer, self-taught, dedicated, teamwork, problem-solving, experience, calm, pressure, remote work, JavaScript, TypeScript, HTML5, CSS, Sass, React JS, StyledComponents, Gamification"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
-        <meta name="author" content="Camilo Pinzon" />
-        <meta name="Copyright" content="Camilo Pinzon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="https://camilopinzon.tech/images/profile-pic.webp" />
-        <link rel="canonical" href="https://camilopinzon.tech" />
+		/>
+		<meta name="robots" content="index, follow" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="language" content="English" />
+		<meta name="author" content="Camilo Pinzon" />
+		<meta name="Copyright" content="Camilo Pinzon" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta property="og:url" content="https://camilopinzon.tech" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content={tilte} />
+		<meta
+			property="og:description"
+			content="Dedicated and self-taught web developer with the ability to assimilate work under pressure and great facility for teamwork. Bringing experience to find solutions for all project stages and calm to the customer."
+		/>
+		<meta
+			property="og:image"
+			content="https://camilopinzon.tech/images/profile-pic.webp"
+		/>
+		<link
+			rel="icon"
+			type="image/png"
+			href="https://camilopinzon.tech/images/profile-pic.webp"
+		/>
+		<link rel="canonical" href="https://camilopinzon.tech" />
 	</Helmet>
 );
