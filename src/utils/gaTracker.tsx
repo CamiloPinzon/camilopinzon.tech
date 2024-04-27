64581cdf1a8cd5ac7b6439cb274e4a2a7fb1e9ff
Camilo Pinzon <pinzonac@gmail.com>
2024-04-26 23:59:23 -0500

    GA changes

--- src/utils/gaTracker.tsx
+++ src/utils/gaTracker.tsx
@@ -1,11 +1,9 @@
 import { useEffect } from "react";
 import ReactGA from "react-ga";
-import { useParams } from "react-router-dom";
 
 const GATracker = () => {
-    const path = useParams();
     useEffect(() => {
-        ReactGA.pageview(window.location.pathname+path + window.location.search);
+        ReactGA.pageview(window.location.pathname + window.location.search);
     }, [])
 }
 
